7deb262ad44f37c2b39ae4900de175b22bd8e975
Flávio Henrique <chakcpps@gmail.com>
2022-12-03 16:08:40 -0300

    :rocket: Criando e adicionando produtos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,100 @@
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons" ;
+import{
+  Box,
+  Flex,
+  Button,
+  useDisclosure,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import ModalComp from "./components/ModalComp";
+
 const App = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [data, setData] = useState([]);
+  const [dataEdit, setDataEdit] = useState([]);
+
+  const isMobile = useBreakpointValue({
+    base: true,
+    lg: false,
+  })
+
+  useEffect(() => {
+    const db_costumer = localStorage.getItem("cad_product")
+      ? JSON.parse(localStorage.getItem("cad_product"))
+      : [];
+
+      setData(db_costumer)
+  }, [setData]);
+
+  const handleRemove = (name) => {
+    const newArray = data.filter((item) => item.name !== name);
+
+    setData(newArray);
+
+    localStorage.setItem("cad_product", JSON.stringify(newArray))
+  };
+
   return (
-    <h1>Hello</h1>
+    <Flex
+      h='100vh'
+      align='center'
+      justify='center'
+      fontSize='20px'
+      fontFamily='poppins'
+      >
+        <Box maxW={800} w='100%' h='100vh' py={10} px={2}>
+          <Button colorScheme='blue' onClick={()=> [setDataEdit({}), onOpen()]}>
+            Adicionar Produto
+          </Button>
+          
+          <Box overflowY='auto' height='100%'>
+            <Table mt='6'>
+              <Thead>
+                <Tr>
+                  <Th maxW={isMobile ? 5:100} fontSize='20px'>Nome do Produto</Th>
+                  <Th maxW={isMobile ? 5:100} fontSize='20px'>Descrição do Produto</Th>
+                  <Th p={0}></Th>
+                  <Th p={0}></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data.map(({name, detail}, index) => (
+                  <Tr key={index} cursor='pointer' _hover={{bg: 'gray.100'}}>
+                    <Td maxW={isMobile ? 5:100}>{name}</Td>
+                    <Td maxW={isMobile ? 5:100}>{detail}</Td>
+                    <Td p={0}>
+                      <EditIcon fontSize={20} onClick={() => [
+                        setDataEdit({name, detail, index}), onOpen(),
+                      ]}
+                    />
+                    </Td>
+                    <Td p={0}>
+                      <DeleteIcon fontSize={20} onClick={() => handleRemove(name)}/>
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </Box>
+        </Box>
+        {isOpen && (
+          <ModalComp 
+          isOpen={isOpen}
+          onClose={onClose}
+          data={data}
+          setData={setData}
+          dataEdit={dataEdit}
+          setDataEdit={setDataEdit}
+          />
+        )}
+    </Flex>
   );
 }
 
--- src/components/ModalComp.jsx
+++ src/components/ModalComp.jsx
@@ -1,40 +1,89 @@
 import React from 'react';
-import AddProducts from '../Pages/AddProducts';
 import { 
     Modal,
-    ModalOver1ay ,
+    ModalOverlay ,
     ModalContent,
     ModalHeader,
     ModalFooter,
     ModalBody,
-    ModalC10seButton,
     Button,
-    FormContr01,
-    FormLabe1,
+    FormControl,
     Input,
     Box,
-    ModalCloseButton, 
+    ModalCloseButton,
+    FormLabel, 
 } from '@chakra-ui/react';
 import { useState } from 'react';
-
-import { FormControl } from '@mui/material';
 
 const ModalComp = ({data, setData, dataEdit, isOpen, onClose}) => {
     const [name, setName] = useState(dataEdit.name || '');
     const [detail, setDetail] = useState(dataEdit.detail || '');
 
+    const handleSave = () => {
+        if(!name || !detail) return;
+
+        if(nameAlreadyExists()){
+            return alert("Produto já cadastrado!")
+        }
+        if(Object.keys(dataEdit).length){
+            data[dataEdit.index] = {name, detail};
+        }
+
+        const newDataArray = !Object.keys(dataEdit).length
+            ? [...(data ? data : []), {name, detail}]
+            : [...(data ? data : [])];
+
+        localStorage.setItem('cad_product', JSON.stringify(newDataArray));
+
+        setData(newDataArray);
+
+        onClose();
+    }
+
+    const nameAlreadyExists = () =>{
+        if(dataEdit.name!== name && data?.lenght){
+            return data.find((item) => item.name === name);
+        }
+    }
+
+
+
     return (  
         <>
             <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOver1ay/>
+                <ModalOverlay/>
                 <ModalContent>
                     <ModalHeader>Cadastrar Produto</ModalHeader>
                     <ModalCloseButton/>
                     <ModalBody>
-                        <FormControl display>
-
+                        <FormControl display='flex' flexDir='column' gap={4}>
+                            <Box>
+                                <FormLabel>Nome do Produto</FormLabel>
+                                <Input
+                                    type='text'
+                                    value={name}
+                                    onChange={(e)=> setName(e.target.value)}
+                                />
+                            </Box>
+                            <Box>
+                                <FormLabel>Descrição do Produto</FormLabel>
+                                <Input
+                                    type='text'
+                                    value={detail}
+                                    onChange={(e)=> setDetail(e.target.value)}
+                                />
+                            </Box>
                         </FormControl>
                     </ModalBody>
+
+                    <ModalFooter justifyContent='start'>
+                        <Button colorScheme='green' mr={3} onClick={handleSave}>
+                            Salvar
+                        </Button>
+                        <Button colorScheme='red' onClick={onClose}>
+                            Cancelar
+                        </Button>
+                    </ModalFooter>
                 </ModalContent>
             </Modal>
         </>
